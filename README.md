7074785c9091f48902993fb309ae75ea5c1cc7c4
Unknown <huw@blooware.co.uk>
2018-04-20 16:37:10 +0100

    Removed Arc Reactor. And Jarvis Keyword wake

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ Credit goes to the respective API creators.
 
 Watch the tutorial series at https://www.youtube.com/HuwProsser
 
-NOTES
+NOTES:
 
 Installing Node.js can be confusing, frustrating and time consuming so I suggest you search 'install node.js on Windows/OSX/Linux'
 
 This will only work in Chrome as it uses Google Speech Recognition
+
+The first time you speak to Jarvis it may take a minute to connect to the Dialogflow Server thing.
 
 To run this:
 
@@ -22,3 +24,10 @@ On Windows Powershell :
 On Linux : 
     1. cd YOUR-DIRECTORY-PATH-FOR-NODEJS/JARVIS
     2. node app.js
+    
+FUTURE PLANS FOR THIS SYSTEM:
+    1. Movie quotes - Jarvis will respond to things he is asked in the movie
+    2. Weather, Time, Date - The boring bits!
+    3. Arduino Compatability
+    4. Database compatability for custom Node.js requests before it get's into Google's mits!
+    4. Anything you guys request!
--- public/js/speech.js
+++ public/js/speech.js
@@ -3,6 +3,8 @@ var botui = new BotUI('botui-app');
 var continuous = false;
 var query = getQueryParams(document.location.search);
 
+
+
 function getQueryParams(qs) {
     qs = qs.split('+').join(' ');
 
@@ -17,7 +19,7 @@ function getQueryParams(qs) {
     return params;
 }
 window.onload = function() {
-    
+
     /*load speech API*/
     var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
     var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
@@ -26,6 +28,55 @@ window.onload = function() {
     var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
     var recognition = new SpeechRecognition();
     var speechRecognitionList = new SpeechGrammarList();
+
+
+
+    // Start listening, don't restart automatically
+    if (annyang) {
+        var commands = {
+            'jarvis': function() {
+                bangOnGooglesDoor();
+            },
+            'hey jarvis': function() {
+                bangOnGooglesDoor();
+            },
+            'oh jarvis': function() {
+                bangOnGooglesDoor();
+            },
+            'um jarvis': function() {
+                bangOnGooglesDoor();
+            },
+            'yo jarvis': function() {
+                bangOnGooglesDoor();
+            },
+            'oi jarvis': function() {
+                bangOnGooglesDoor();
+            },
+            'ok jarvis': function() {
+                bangOnGooglesDoor();
+            },
+            'listen to me': function() {
+                bangOnGooglesDoor();
+            },
+            "I'm talking to you jarvis": function() {
+                bangOnGooglesDoor();
+            }
+        };
+        annyang.addCommands(commands);
+        annyang.start();
+    } //listens for Jarvis to begin Google rec
+
+
+    function bangOnGooglesDoor(){
+        annyang.abort();
+
+        botui.message.add({
+            content: 'How can I help?'
+        }); //add to conversation UI
+        var audio = new Audio('wake.wav');
+        audio.play();  
+        setTimeout(startListening,500);
+    }
     speechRecognitionList.addFromString(grammar, 1);
     recognition.grammars = speechRecognitionList;
     recognition.continuous = false;
@@ -38,16 +89,14 @@ window.onload = function() {
             content: message.data
         }); //add to conversation UI
         responsiveVoice.speak(message.data); //say response     
-        if(continuous){
-            startListening();
-        } //if continuous - listen again EXPERIMENTAL
+        annyang.start();
     }); //on message from server
 
     document.body.onclick = function() {
 
-        startListening();
+
     } //on page click start listening
-    
+
     recognition.onresult = function(event) {
         var last = event.results.length - 1;
         var input = event.results[last][0].transcript;
@@ -81,7 +130,7 @@ window.onload = function() {
     } //on no match DO SOMETHING
 
     recognition.onerror = function(event) {
-        diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+        console.log('Error occurred in recognition: ' + event.error);
     } //on error DO SOMETHING
 
     function startListening(){
